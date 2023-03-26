558478e552e556037e819834af77873245872b04
Mordechai <mordwein77@gmail.com>
2023-03-26 17:21:24 -0400

    commit tree 4b4912234f2fe2c0d033eda4d00af33574d630db

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,36 @@
 class ItemsController < ApplicationController
 
-  def index
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def index 
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
     items = Item.all
+    end
     render json: items, include: :user
   end
+    
+    def show
+      item = Item.find(params[:id])
+      render json: item
+    end
+
+    def create
+      user = User.find(params[:user_id])
+      item = user.items.create(strong_params)
+      render json: item, status: :created
+    end
+
+    private
+
+    def strong_params
+      params.permit(:name, :description, :price)
+    end
+
+    def render_not_found_response
+      render json: { error: "item not found" }, status: :not_found
+    end
 
 end
